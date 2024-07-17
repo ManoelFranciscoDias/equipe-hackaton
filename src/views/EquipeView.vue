63243a27e7d6cee871a940f4c5b0e012a791eca7
manoel2007dias@gmail.com <manoel2007dias@gmail.com>
2024-07-17 09:28:17 -0300

    first commit

---
+++ src/views/EquipeView.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    name: 'EquipeView',
+    data() {
+        return {
+            equipe: [
+                {
+                    nome: 'João Pedro Dums (Santos)',
+                    cargo: 'Back-End'
+                },
+                {
+                    nome: 'Matheus Gaspar (Pito)',
+                    cargo: 'Back-End / Geral'
+                },
+                {
+                    nome: 'Enrique Cardoso (Doso)',
+                    cargo: 'Front-End (auxiliar)'
+                },
+                {
+                    nome: 'Nicolas Sestrem (Kito)',
+                    cargo: 'Figma'
+                },
+                {
+                    nome: 'Manoel Dias (Nel)',
+                    cargo: 'Front-End (principal)'
+                },
+                {
+                    nome: 'Kaue (Doka)',
+                    cargo: 'RNF, RF, RN'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Equipe</h1>
+        <ul>
+            <li v-for="membro in equipe" :key="membro.nome">
+                <h2>{{ membro.nome }}</h2>
+                <p>{{ membro.cargo }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+/* Your component-specific styles go here */
+</style>
